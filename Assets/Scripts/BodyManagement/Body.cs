42f21bb9ba353d7e082007052b1766f546a64260
DESKTOP-1UG72O1\Nico <sjorspnk@gmail.com>
2022-10-10 15:40:35 +0200

    More formatting

--- Assets/Scripts/BodyManagement/Body.cs
+++ Assets/Scripts/BodyManagement/Body.cs
@@ -18,23 +18,6 @@ public class Body : MonoDodge
     private bool isGrounded = false;
     private bool isActive = false;
 
-    // When spawning reset all values and check wether it needs to start falling eventually or not
-    public void Spawn(Transform target) 
-    {
-        gravity = 0f;
-        transform.position = target.position;
-        transform.rotation = target.rotation;
-        particles.Play();
-        fade.Play(0);
-        currentTime = 0f;
-
-        //Problematic when spawning on top of another statue as that will eventually fall and this won't
-        //It's a small detail but this is better on performance like this,
-        //Maybe a re-check event could work? But it's fine for a prototype! :D
-        isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.5f, enviroment);
-        isActive = true;
-    }
-
     // Check whether has started and not on the ground in order to check the floating time
     public override void Update() 
     {
@@ -51,5 +34,22 @@ public class Body : MonoDodge
                 gravity -= 0.01f * Time.deltaTime;
             }
         }
+    }
+
+    // When spawning reset all values and check wether it needs to start falling eventually or not
+    public void Spawn(Transform target) 
+    {
+        gravity = 0f;
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+        particles.Play();
+        fade.Play(0);
+        currentTime = 0f;
+
+        //Problematic when spawning on top of another statue as that will eventually fall and this won't
+        //It's a small detail but this is better on performance like this,
+        //Maybe a re-check event could work? But it's fine for a prototype! :D
+        isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.5f, enviroment);
+        isActive = true;
     }
 }
